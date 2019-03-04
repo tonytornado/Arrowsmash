28e7c844e71b863b92de703321dfcdfc62561572
Tony Thigpen <tonytatsumaki@live.com>
2019-03-04 18:28:54 -0500

    Apparently, this is alright looking.

--- Accounts/models.py
+++ Accounts/models.py
@@ -29,52 +29,6 @@ def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 
-class Profile(models.Model):
-    """    Main profile class that needs all the work.    """
-    user = models.OneToOneField(User, models.CASCADE)
-    avatar = models.ImageField(upload_to=user_directory_path, default='default.jpg')
-    rival_code = models.CharField(max_length=9, blank=True)
-    tagline = models.CharField(max_length=100, blank=True, help_text="It's your life. What's its tag line?")
-    DOB = models.DateField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, default="NB", max_length=2, help_text='We have to ask.')
-    city = models.CharField(max_length=30, blank=True)
-    state = models.CharField(max_length=2, blank=True)
-    bio = models.TextField(max_length=5000, blank=True)
-
-    # blog = models.OneToOneField(Blog, models.CASCADE)
-
-    @property
-    def age(self):
-        """Returns the age of a user"""
-        return datetime.date.today().year - self.DOB.year
-
-    @property
-    def full_name(self):
-        """Returns a full name"""
-        return "{} {}".format(self.user.first_name, self.user.last_name)
-
-    @receiver(post_save, sender=User)
-    def update_user_profile(sender, instance, created, **kwargs):
-        """Updates the profile for someone"""
-        if created:
-            Profile.objects.create(user=instance)
-        instance.profile.save()
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        """Saves the profile for someone"""
-        instance.profile.save()
-
-    @property
-    def quix(request):
-        qs = Follow.objects.filter(followee=request.user.profile).all()
-        following = [u.follower_id for u in qs]
-        return following
-
-    def __str__(self):
-        return "{} [{} {}]".format(self.user, self.user.first_name, self.user.last_name)
-
-
 class FollowManager(models.Manager):
     @staticmethod
     def follow(follower, followee):
@@ -113,6 +67,48 @@ class FollowManager(models.Manager):
         qs = Follow.objects.filter(followee=pro).all()
         followers = [u.follower for u in qs]
         return followers
+
+
+class Profile(models.Model):
+    """    Main profile class that needs all the work.    """
+    user = models.OneToOneField(User, models.CASCADE)
+    avatar = models.ImageField(upload_to=user_directory_path, default='default.jpg')
+    rival_code = models.CharField(max_length=9, blank=True)
+    tagline = models.CharField(max_length=100, blank=True, help_text="It's your life. What's its tag line?")
+    DOB = models.DateField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, default="NB", max_length=2, help_text='We have to ask.')
+    city = models.CharField(max_length=30, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    bio = models.TextField(max_length=5000, blank=True)
+
+    objects = models.Manager()
+
+    # blog = models.OneToOneField(Blog, models.CASCADE)
+
+    @property
+    def age(self):
+        """Returns the age of a user"""
+        return datetime.date.today().year - self.DOB.year
+
+    @property
+    def full_name(self):
+        """Returns a full name"""
+        return "{} {}".format(self.user.first_name, self.user.last_name)
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        """Updates the profile for someone"""
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        """Saves the profile for someone"""
+        instance.profile.save()
+
+    def __str__(self):
+        return "{} [{} {}]".format(self.user, self.user.first_name, self.user.last_name)
 
 
 class Follow(models.Model):
